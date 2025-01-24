5bc2c92b92f14ec7d24a15495f894ceb9abcb585
uday4002 <149502249+uday4002@users.noreply.github.com>
2025-01-24 09:16:37 +0530

    Update index.js

--- client/src/components/AddBlog/index.js
+++ client/src/components/AddBlog/index.js
@@ -32,7 +32,7 @@ const AddBlog = () => {
     const onFormSubmit = async(e) =>{
         e.preventDefault()
         const token = Cookies.get('jwt_token')
-        const response = await axios.post(`http://localhost:3001/blogs`,{title,content},{
+        const response = await axios.post(`https://nodejsblogmanagement-backend.onrender.com`,{title,content},{
             headers:{
                 Authorization:`Bearer ${token}`
             }
