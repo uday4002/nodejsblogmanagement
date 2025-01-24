4cd2a01673932212b66aa40241f462d6c58fca2f
uday4002 <149502249+uday4002@users.noreply.github.com>
2025-01-24 15:55:28 +0530

    Update index.js

--- client/src/components/CommentsSection/index.js
+++ client/src/components/CommentsSection/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Cookies from 'js-cookie;
+import Cookies from 'js-cookie';
 
 const CommentsSection = ({ blogId, token }) => {
   const [comments, setComments] = useState([]);
